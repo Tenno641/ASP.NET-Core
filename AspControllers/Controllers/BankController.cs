e47599cd89e357f992b6f1dd5f2f53f9c18227fe
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2025-12-17 12:00:20 +0200

    Simple Controller

---
+++ AspControllers/Controllers/BankController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspControllers.Controllers;
+
+public class BankController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return Ok(new { Message = "Welcome to the bank" });
+    }
+
+    [Route("account-details")]
+    public IActionResult AccountDetails()
+    {
+        return Ok(new { AccountNumber = 100, AccountHolderName = "User-Defined", CurrentBalance = 5000 });
+    }
+
+    [Route("account-statement")]
+    public IActionResult AccountStatement()
+    {
+        return File("text.pdf", "application/pdf");
+    }
+
+    [Route("get-current-balance/{accountNumber:int?}")]
+    public IActionResult GetCurrentBalance()
+    {
+        if (!Request.RouteValues.ContainsKey("accountNumber")) return NotFound("Account number should be supplided");
+        int accountNumber = Convert.ToInt32(Request.RouteValues["accountNumber"]);
+        if (accountNumber == 1001) return Ok(5000);
+        else return BadRequest("Account number should be 1001");
+    }
+}
---
+++ AspControllers/Controllers/CustomController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspControllers.Controllers;
+
+public class CustomController : Controller
+{
+    [Route("welcome")]
+    public IActionResult Method()
+    {
+        return new ContentResult()
+        {
+            Content = """
+            {
+              "name": "User-Name"
+            }
+            """,
+            ContentType = "application/json"
+        };
+    }
+
+    [Route("person")]
+    public JsonResult Person()
+    {
+        return new JsonResult(new { Id = Guid.NewGuid(), FirstName = "User-First-Name", LastName = "User-Last-Name", Age = 23 });
+        // return Json(person);
+    }
+}
