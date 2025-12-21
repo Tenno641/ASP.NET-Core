33a8ee103aaedc92e081e67d7127983ff174e7dd
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2025-12-21 14:10:41 +0200

    Razor Layout

---
+++ AspRazorLayout/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspRazorLayout.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("about-company")]
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    [Route("contact-company")]
+    public IActionResult Contact()
+    {
+        return View();
+    }
+}
---
+++ AspRazorLayout/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspRazorLayout.Controllers;
+
+public class ProductsController : Controller
+{
+    [Route("products")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("search-products")]
+    public IActionResult Search()
+    {
+        return View();
+    }
+
+    [Route("order-product")]
+    public IActionResult Order()
+    {
+        return View();
+    }
+}
