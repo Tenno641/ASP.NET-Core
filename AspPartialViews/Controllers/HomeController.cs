2f6b962e76db4ec026e839f8100aa2b24fc802ea
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2025-12-21 21:17:38 +0200

    Calling Partial Views asynchronously

---
+++ AspPartialViews/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using AspPartialViews.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspPartialViews.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        ViewData["appTitle"] = "Titly";
+        ViewData["listy"] = new List<string>() { "USA", "GERMANY", "FRANCE", "SPAIN", "COLOMBIA" };
+        return View();
+    }
+
+    [Route("programming-languages")]    
+    public IActionResult GetProgrammingLanguages()
+    {
+        ListModel listModel = new ListModel() { Title = "Programming Languages", Items = ["C#", "TypeScript", "Go", "Rust"] };
+        return PartialView("_ListView", listModel);
+    }
+    
+}
---
+++ AspPartialViews/Models/ListModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AspPartialViews.Models;
+
+public class ListModel
+{
+    public string Title { get; init; }
+    public IEnumerable<string> Items { get; init; }
+}
